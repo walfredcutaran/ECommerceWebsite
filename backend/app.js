49934eadd1fb949f809bbd3859bc6ecb6720b212
walfredcutaran <100104566+walfredcutaran@users.noreply.github.com>
2024-03-13 00:58:42 +0800

    deployment test

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 
 const cookieParser = require('cookie-parser');
+const path = require('path');
 
 const errorMiddleware = require('./middlewares/errors');
 
@@ -14,6 +15,14 @@ const auth = require('./routes/auth');
 
 app.use('/api/v1', products)
 app.use('/api/v1', auth)
+
+if (process.env.NODE_ENV == 'PRODUCTION') {
+    app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, '../frontend/build/index.html'));
+    })
+}
 
 // middleware to handle errors
 app.use(errorMiddleware);
